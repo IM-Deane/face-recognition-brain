0a0851d5df2070b530ba50e4cc3a19aa50347fc2
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2021-07-27 16:59:15 -0600

    Implemented backend server to persist data.

--- smart-brain-api/controllers/user.controllers.js
+++ smart-brain-api/controllers/user.controllers.js
@@ -1,73 +1,90 @@
-// Mock user database
-import { users } from "../constants/user.data.js";
+import db from "../server.js";
 import bcrypt from "bcryptjs";
 
 export const getUsers = (req, res) => {
-	res.json(users);
+	res.json("Hello from server");
 };
 
 export const getUserProfile = (req, res) => {
 	const { id } = req.params;
-	let isFound = false;
 
-	users.forEach((user) => {
-		if (user.id === id) {
-			isFound = true;
-			return res.json(user);
-		}
-	});
-	if (!isFound) {
-		res.status(404).json("User not found");
-	}
+	// Get user by their id from db
+	db.select("*")
+		.from("users")
+		.where({ id })
+		.then((user) =>
+			user.length ? res.json(user[0]) : res.status(404).json("User not found")
+		)
+		.catch((err) => res.status(400).json("Error getting user."));
 };
 
 export const signInUser = (req, res) => {
 	const { email, password } = req.body;
 
-	// Search for user in database
-
-	// const user = users.findIndex((user) => user.email === email);
-
-	// // Check hashed password
-	// console.log(bcrypt.compareSync(user.password, password));
-
-	if (email === users[0].email && password === users[0].password) {
-		res.json(users[0]);
-	} else {
-		res.status(400).json("Error logging in.");
-	}
+	db.select("email", "hash")
+		.from("login")
+		.where("email", "=", email)
+		.then((data) => {
+			const isValid = bcrypt.compareSync(password, data[0].hash);
+			if (isValid) {
+				return db
+					.select("*")
+					.from("users")
+					.where("email", "=", email)
+					.then((user) => res.json(user[0]))
+					.catch((err) => res.status(400).json("Unable to get user."));
+			} else {
+				res.status(400).json("Wrong credentials.");
+			}
+		})
+		.catch((err) => res.status(400).json("Wrong credentials."));
 };
 
 export const registerUser = (req, res) => {
 	const { email, name, password } = req.body;
 
 	// Hash user password
-	// const hashedPassword = bcrypt.hashSync("bacon", 10);
+	const hash = bcrypt.hashSync(password, 10);
 
-	users.push({
-		id: "125",
-		name,
-		email,
-		entries: 0,
-		joined: new Date(),
+	// Transaction will roll back insert operation if any error
+	// is encountered.
+	db.transaction((trx) => {
+		trx
+			.insert({
+				hash,
+				email,
+			})
+			.into("login")
+			// Returning = provides result of last insert
+			.returning("email")
+			.then((loginEmail) => {
+				return (
+					trx("users")
+						.returning("*")
+						.insert({
+							email: loginEmail[0],
+							name,
+							joined: new Date(),
+						})
+						.then((user) => {
+							res.json(user[0]);
+						})
+						// Commit or rollback transaction
+						.then(trx.commit)
+						.catch(trx.rollback)
+				);
+			})
+			.catch((err) => res.status(400).json("Unable to register."));
 	});
-
-	res.json(users[users.length - 1]);
 };
 
 export const updateUserEntries = (req, res) => {
 	const { id } = req.body;
-	let isFound = false;
 
-	users.forEach((user) => {
-		if (user.id === id) {
-			isFound = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	});
-
-	if (!isFound) {
-		res.status(404).json("User not found");
-	}
+	db("users")
+		.where("id", "=", id)
+		.increment("entries", 1)
+		.returning("entries")
+		.then((entries) => res.json(entries[0]))
+		.catch((err) => res.status(400).json("Unable to retrieve entries."));
 };
--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
 import users from "./routes/user.routes.js";
+import knex from "knex";
 
 const app = express();
 const PORT = 3000;
 
 // Middlewares
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
@@ -14,6 +14,19 @@ app.use(cors());
 // Routes
 app.use(users);
 
+// DB Connection
+const db = knex({
+	client: "pg",
+	connection: {
+		host: "127.0.0.1",
+		user: "tdeane",
+		password: "Masterlock",
+		database: "smart-brain",
+	},
+});
+
 app.listen(PORT, () => {
 	console.log(`App is running on port ${PORT}`);
 });
+
+export default db;
